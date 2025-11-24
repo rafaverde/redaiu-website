0a9e2eeb63e4bc28564748ad876ded374bb6a233
Rafael Valverde <rafaverde@msn.com>
2025-11-24 10:06:15 -0300

    feat: implement new footer and header components with responsive design
    - Remove unused SVG files: vercel.svg and window.svg.
    - Add Footer component to display company information, navigation links, and contact details.
    - Create Header component with responsive navigation and mobile menu functionality.
    - Introduce Logo component for consistent branding.
    - Implement reusable Button and Sheet components for UI consistency.
    - Update global API fetching logic to include additional image details.
    - Define navigation items in constants for easier management.
    - Add utility function for class name merging.
    - Update global types to reflect changes in API response structure.

---
+++ src/components/Header.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Logo from "./layout/Logo";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import { NAV_ITEMS } from "@/src/lib/constants";
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const pathname = usePathname();
+
+  // Fecha menu mobile ao mudar de rota
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
+  // Controla o Scroll para comportamento do Header
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 20);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <header className=" w-full sticky top-0">
+      <div className="w-full bg-redaiu-blue-700 shadow-sm shadow-redaiu-gray-800">
+        <div className="container m-auto p-5 md:px-0 flex justify-between items-center gap-8 md:gap-2">
+          <Link href="/">
+            <Logo isCompact={isScrolled} />
+          </Link>
+
+          {/* Desktop Menu */}
+          <nav className="hidden md:flex items-center gap-8 ">
+            {NAV_ITEMS.map((item, index) => {
+              const isActive = pathname === item.href;
+              return (
+                <Link
+                  key={index}
+                  href={item.href}
+                  className={`hover:text-redaiu-blue-300 transition-colors duration-500 ${
+                    isActive ? "text-redaiu-blue-300 font-medium" : ""
+                  }`}
+                >
+                  {item.label}
+                </Link>
+              );
+            })}
+          </nav>
+
+          {/* Mobile Menu Sheet*/}
+          <div className="md:hidden">
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger asChild>
+                <Button
+                  asChild
+                  size="icon"
+                  aria-label="Abir menu"
+                  className="bg-redaiu-blue-700"
+                >
+                  <Menu className="text-redaiu-blue-100 w-8 h-8" />
+                </Button>
+              </SheetTrigger>
+
+              <SheetContent
+                side="top"
+                className="w-full h-full bg-redaiu-blue-700/90"
+              >
+                <SheetTitle className="sr-only">Menu</SheetTitle>
+                <SheetDescription className="sr-only">
+                  Menu de Navegación Mobile
+                </SheetDescription>
+                <nav className="flex flex-col items-center justify-center h-full gap-8 mt-8">
+                  {NAV_ITEMS.map((item, index) => {
+                    const isActive = pathname === item.href;
+                    return (
+                      <Link
+                        key={index}
+                        href={item.href}
+                        onClick={() => setIsOpen(false)} // Fecha ao clicar
+                        className={`text-2xl font-medium transition-colors ${
+                          isActive
+                            ? "text-redaiu-blue-300 font-bold"
+                            : "text-white hover:text-redaiu-blue-300"
+                        }`}
+                      >
+                        {item.label}
+                      </Link>
+                    );
+                  })}
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,12 +1,26 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 let client: ApolloClient | null = null;
 
 export const getClient = () => {
+  const apiUrl = process.env.NEXT_PUBLIC_WORDPRESS_API_URL;
+
+  console.log("🔗 Conectando ao WordPress em:", apiUrl);
+
+  if (!apiUrl) {
+    throw new Error(
+      "❌ Erro Fatal: A variável NEXT_PUBLIC_WORDPRESS_API_URL não está definida"
+    );
+  }
+
+  if (typeof window === "undefined" && process.env.NODE_ENV === "development") {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  }
+
   if (!client || typeof window === "undefined") {
     client = new ApolloClient({
       link: new HttpLink({
-        uri: process.env.NEXT_PUBLIC_WORDPRESS_API_LINK,
+        uri: apiUrl,
       }),
       cache: new InMemoryCache(),
     });
