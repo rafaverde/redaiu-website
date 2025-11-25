1a5da548bb795d7239c36a82158439c539042b5b
Rafael Valverde <rafaverde@msn.com>
2025-11-25 15:27:16 -0300

    feat: add menu API and types for company menu data fetching

---
+++ src/lib/api/menu.ts
@@ -0,0 +1,35 @@
+import { gql } from "@apollo/client";
+import { getClient } from "../apollo";
+import { CompanyMenuResponse } from "@/src/types/menu";
+
+export const GET_MENU_COMPANIES = gql`
+  query GetMenuCompanies {
+    empresas(first: 100, where: { orderby: { field: TITLE, order: ASC } }) {
+      nodes {
+        title
+        slug
+        categoriasEmpresa {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+export async function getMenuCompanies() {
+  const client = getClient();
+
+  try {
+    const { data } = await client.query<CompanyMenuResponse>({
+      query: GET_MENU_COMPANIES,
+      fetchPolicy: "no-cache",
+    });
+
+    return data?.empresas.nodes || [];
+  } catch (err) {
+    console.error("Erro ao buscar empresas do menu:", err);
+    return [];
+  }
+}
